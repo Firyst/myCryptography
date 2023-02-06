2c32233b68000ac441b15372a9097c78aaadf1d9
Phil <filipp.0104@mail.ru>
2023-02-07 01:52:26 +0300

    affine decrypt

--- cryptos/affine.py
+++ cryptos/affine.py
@@ -16,7 +16,6 @@ class Crypto(QWidget):
 
         self.auto_button.clicked.connect(self.auto_alph)
 
-
     def auto_alph(self):
         alph = ''.join(sorted(list(set(self.parent_window.open_text()))))
         self.alph0.setText(alph)
@@ -31,12 +30,38 @@ class Crypto(QWidget):
         if text:
             self.alph1.setText(text[-1] + text[:-1])
 
+    def decrypt(self):
+        alph = list(self.alph0.text())
+        alph_rev = dict(zip(alph, range(len(alph))))  # создать обратный словарь для более быстрой работы
+        try:
+            a, b = int(self.key_a.value()), int(self.key_b.value())
+            ia = pow(a, -1, len(alph))
+        except ValueError:
+            dialog = WarnDialog("Ошибка", "Ключи заданы неверно")
+            dialog.exec_()
+            return ''
+        if math.gcd(len(alph), a) == 1:
+            # crypto begin
+            decrypted = ""
+            s = "?"
+            try:
+                for s in self.parent_window.cipher_text():
+                    decrypted += alph[((alph_rev[s] - b) * ia) % len(alph)]  # формула
+            except KeyError:
+                dialog = WarnDialog("Ошибка", f"Символ <{s}> отсутсвует в заданном алфавите.")
+                dialog.exec_()
+                return ""
+            return decrypted
+        else:
+            dialog = WarnDialog("Ошибка", "Числа a и m должны быть взаимно простыми")
+            dialog.exec_()
+            return ''
+
     def encrypt(self):
         alph = list(self.alph0.text())
         alph_rev = dict(zip(alph, range(len(alph))))  # создать обратный словарь для более быстрой работы
         print(alph, alph_rev)
         try:
-            print
             a, b = int(self.key_a.value()), int(self.key_b.value())
         except ValueError:
             dialog = WarnDialog("Ошибка", "Ключи заданы неверно")
@@ -56,24 +81,5 @@ class Crypto(QWidget):
             return encrypted
         else:
             dialog = WarnDialog("Ошибка", "Числа a и m должны быть взаимно простыми")
-            dialog.exec_()
-            return ''
-
-    def decrypt(self):
-        alph1, alph0 = self.alph0.text(), self.alph1.text()
-        if len(alph0) == len(alph1):
-            decrypted = ""
-            replaces = dict(zip(alph0, alph1))
-            s = "?"
-            try:
-                for s in self.parent_window.cipher_text():
-                    decrypted += replaces[s]
-            except KeyError:
-                dialog = WarnDialog("Ошибка", f"Символ <{s}> отсутсвует в заданном алфавите.")
-                dialog.exec_()
-                return ""
-            return decrypted
-        else:
-            dialog = WarnDialog("Ошибка", "Длина алфавитов не совпадает")
             dialog.exec_()
             return ''
